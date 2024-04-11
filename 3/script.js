0d253bb29753eb811684ddc5d345c8b6966ed1a9
giftpad-mainali-shital <shital.mainali@giftpad.jp>
2024-04-11 11:21:25 +0900

    animation delay

---
+++ 3/script.js
@@ -0,0 +1,16 @@
+gsap.to(".green", { 
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: 'none'
+});
+
+
+gsap.to(".orange", { 
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: 'bounce.out'
+});
---
+++ 4/script.js
@@ -0,0 +1,14 @@
+gsap.to(".green", { 
+  rotation: 360,
+  duration: 1,
+});
+gsap.to(".purple", { 
+  rotation: 360,
+  duration: 1,
+  delay: 1,
+});
+gsap.to(".orange", { 
+  rotation: 360,
+  duration: 1,
+  delay: 2,
+});
